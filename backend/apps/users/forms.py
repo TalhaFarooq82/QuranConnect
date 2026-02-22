dbd8c2908d47535f3ded589b14b0e99215dd13df
TalhaFarooq82 <talha.farooq6625@gmail.com>
2026-02-22 13:19:51 +0500

    Add username field to signup forms and HTML

--- backend/apps/users/forms.py
+++ backend/apps/users/forms.py
@@ -5,7 +5,7 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ['first_name', 'last_name', 'email', 'password1', 'password2', 'gender']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'gender']
 
     def save(self, commit=True):
         user = super().save(commit=False)
