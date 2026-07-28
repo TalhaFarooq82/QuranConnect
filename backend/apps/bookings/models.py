99f1246b027cac694d2df78039d6e769e0261df4
Laiba3126 <laiba.bashir034@gmail.com>
2026-07-29 02:02:23 +0500

    Update models.py

--- backend/apps/bookings/models.py
+++ backend/apps/bookings/models.py
@@ -73,6 +73,7 @@ class Proposal(models.Model):
     proposal_text = models.TextField()
     rating = models.DecimalField(max_digits=2, decimal_places=1, default=4.9)
     reviews = models.IntegerField(default=0)
+
     availability = models.CharField(max_length=100, default="Available Immediately")
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="Pending")
     created_at = models.DateTimeField(auto_now_add=True)
