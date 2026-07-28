788c0067b1353301bfb52ae0914605029652f29c
Laiba3126 <laiba.bashir034@gmail.com>
2026-07-29 02:00:41 +0500

    Update models.py

--- backend/apps/bookings/models.py
+++ backend/apps/bookings/models.py
@@ -2,7 +2,8 @@ from django.conf import settings
 from django.db import models
 
 class Wallet(models.Model):
-    user = models.OneToOneField(
+    user = models.OneToOneField 
+    (
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         related_name="wallet"
