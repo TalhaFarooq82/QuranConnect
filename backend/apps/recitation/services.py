5af220f35074214cf2d1b674e679af734b4daf8e
Laiba3126 <laiba.bashir034@gmail.com>
2026-07-29 01:28:15 +0500

    Update services.py

--- backend/apps/recitation/services.py
+++ backend/apps/recitation/services.py
@@ -153,7 +153,6 @@ def analyze_recitation(audio_file):
     except Exception as e:
         print("❌ ERROR inside analyze_recitation:", e)
         raise
- 
 def save_recitation(user, audio_file_path, result):
     RecitationAttempt.objects.create(
         user         = user,
